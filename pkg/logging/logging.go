e46e7a83fe4fd8a924ad41ccf5b61f6ad7480782
Michael Robinson <laxdog@gmail.com>
2022-01-22 15:43:55 +0000

    Add dirty Log() func
    Signed-off-by: Michael Robinson <laxdog@gmail.com>

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -19,3 +19,25 @@ func Enable() {
 	registers.MgbaLogging.MGBA_LOG_OUT5.Set(b5)
 	registers.MgbaLogging.MGBA_FLAGS.Set(1 | 0x100)
 }
+
+func Log(a, b, c, d, e, f, g, h byte) {
+	var b1 byte = a
+	var b2 byte = b
+	var b3 byte = c
+	var b4 byte = d
+	var b5 byte = e
+	var b6 byte = f
+	var b7 byte = g
+	var b8 byte = h
+
+	registers.MgbaLogging.MGBA_LOG_OUT1.Set(b1)
+	registers.MgbaLogging.MGBA_LOG_OUT2.Set(b2)
+	registers.MgbaLogging.MGBA_LOG_OUT3.Set(b3)
+	registers.MgbaLogging.MGBA_LOG_OUT4.Set(b4)
+	registers.MgbaLogging.MGBA_LOG_OUT5.Set(b5)
+	registers.MgbaLogging.MGBA_LOG_OUT6.Set(b6)
+	registers.MgbaLogging.MGBA_LOG_OUT7.Set(b7)
+	registers.MgbaLogging.MGBA_LOG_OUT8.Set(b8)
+	registers.MgbaLogging.MGBA_FLAGS.Set(1 | 0x100)
+
+}
