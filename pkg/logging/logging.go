5942d141e7c9ff8c99685767a3c1860c8e5556bf
Matt Mulhern <mattmulhern01@gmail.com>
2022-01-22 16:38:18 +0000

    implement logging.Log
    Signed-off-by: Matt Mulhern <mattmulhern01@gmail.com>

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -6,17 +6,19 @@ import (
 
 func Enable() {
 	registers.MgbaLogging.MGBA_ENABLE.Set(0xc0de)
-	var b1 byte = 'H'
-	var b2 byte = 'E'
-	var b3 byte = 'L'
-	var b4 byte = 'L'
-	var b5 byte = 'O'
+	test_str := "Enabling mGBA logging"
+	str_bytes := []byte(test_str)
+	for idx, char := range str_bytes {
+		registers.MgbaLogging.MGBA_LOG_ADDRESSES[idx].Set(char)
+	}
+	registers.MgbaLogging.MGBA_FLAGS.Set(1 | 0x100)
 
-	registers.MgbaLogging.MGBA_LOG_OUT1.Set(b1)
-	registers.MgbaLogging.MGBA_LOG_OUT2.Set(b2)
-	registers.MgbaLogging.MGBA_LOG_OUT3.Set(b3)
-	registers.MgbaLogging.MGBA_LOG_OUT4.Set(b4)
-	registers.MgbaLogging.MGBA_LOG_OUT5.Set(b5)
+}
+func Log(message string) {
+	str_bytes := []byte(message)
+	for idx, char := range str_bytes {
+		registers.MgbaLogging.MGBA_LOG_ADDRESSES[idx].Set(char)
+	}
 	registers.MgbaLogging.MGBA_FLAGS.Set(1 | 0x100)
 }
 
